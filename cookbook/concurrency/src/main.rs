0edcfce2a2547c34198e7b41ed1c7c568163eaea
chaos-hj <>
2021-05-10 10:03:47 +0800

    julia set

--- cookbook/concurrency/src/main.rs
+++ cookbook/concurrency/src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
-    crossbeam::it_works();
+    // crossbeam::it_works();
+    // match global_state::it_works() {
+    //     Ok(()) => println!("sucessful"),
+    //     Err(res) => println!("{}", res),
+    // }
+
+    draw::it_works();
 }
 
 mod crossbeam {
@@ -115,5 +121,93 @@ mod crossbeam {
             let msg = rcv.recv().unwrap();
             println!("Received {}", msg);
         }
+    }
+}
+
+mod global_state {
+    #![allow(unused)]
+    use lazy_static::lazy_static;
+    use std::sync::Mutex;
+
+    lazy_static! {
+        static ref FRUIT: Mutex<Vec<String>> = Mutex::new(Vec::new());
+    }
+
+    fn insert(fruit: &str) -> Result<(), &str> {
+        let mut db = FRUIT.lock().map_err(|_| "Failed to acquire MutexGuard")?;
+        db.push(fruit.to_string());
+        Ok(())
+    }
+
+    pub fn it_works() -> Result<(), &'static str> {
+        insert("apple")?;
+        insert("orange")?;
+        insert("peach")?;
+        {
+            let db = FRUIT.lock().map_err(|_| "Failed to acquire MutexGuard")?;
+            db.iter()
+                .enumerate()
+                .for_each(|(index, item)| println!("{}: {}", index, item));
+        }
+        insert("graph")?;
+        Ok(())
+    }
+}
+
+mod draw {
+    #![allow(unused)]
+    use image::{ImageBuffer, Pixel, Rgb};
+    use num::complex::Complex;
+    use std::sync::mpsc::{channel, RecvError};
+    use threadpool::ThreadPool;
+
+    pub fn it_works() {
+        let (width, height) = (1920, 1080);
+        let mut img = ImageBuffer::new(width, height);
+        let iterations = 300;
+
+        let c = Complex::new(-0.8, 0.156);
+        let (tx, rx) = channel();
+        let pool = ThreadPool::new(num_cpus::get());
+
+        for y in 0..height {
+            let tx = tx.clone();
+            pool.execute(move || {
+                for x in 0..width {
+                    // let i = julia(c, x, y, width, height, iterations);
+                    // let pixel = wavelength_to_rgb(380 + i * 400 / iterations);
+                    let inner_y = y as f32;
+                    let inner_x = x as f32;
+                    let inner_height = height as f32;
+                    let inner_width = width as f32;
+
+                    let mut zx = 3.0 * (inner_x - 0.5 * inner_width) / (inner_width);
+                    let mut zy = 2.0 * (inner_y - 0.5 * inner_height) / (inner_height);
+
+                    let mut i = iterations;
+
+                    while zx * zx + zy * zy < 4.0 && i > 1 {
+                        let tmp = zx * zx - zy * zy + c.re;
+                        zy = 2.0 * zx * zy + c.im;
+                        zx = tmp;
+                        i -= 1;
+                    }
+
+                    // guesswork to make the rgb color values look okay
+                    let r = (i << 3) as u8;
+                    let g = (i << 5) as u8;
+                    let b = (i * 4) as u8;
+                    let pixel = Rgb::from_channels(r, g, b, 0);
+                    tx.send((x, y, pixel));
+                }
+            });
+        }
+
+        for _ in 0..(width * height) {
+            let (x, y, pixel) = rx.recv().unwrap();
+            img.put_pixel(x, y, pixel);
+        }
+
+        img.save("output.png");
     }
 }
